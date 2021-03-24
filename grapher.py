6b70b2df48b9052e04f47d2e0033b26ba136d31b
Peter <Peter@MacBook-Pro.local>
2021-03-24 10:10:43 +0800

    add plot_dataset function

--- grapher.py
+++ grapher.py
@@ -1,12 +1,14 @@
 from colour import Color
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import qutip as qt
 
 def plot_measurements(vector_alice_list, vector_bob_list):
     num = length(vector_alice_list)
     b = qt.Bloch()
     b.vector_color = list(map(lambda x: x.rgb, list(
-        Color("red").range_to(Color("purple"), num))))
+        Color('red').range_to(Color('purple'), num))))
     for i in range(num):
         vector_alice = vector_alice_list[i]
         vector_bob = vector_bob_list[i]
@@ -14,3 +16,14 @@ def plot_measurements(vector_alice_list, vector_bob_list):
         b.add_vectors(vector_bob)
     b.show()
     plt.show()
+
+
+def plot_dataset(filename, xlabel='X axis', ylabel='Y axis', title='Title'):
+    data = pd.read_csv(filename, index_col=0, names=['parameter', 'loss'])
+    sns.set_theme()
+    g = sns.relplot(x=0, y=1, kind="line", data=data)
+    g.set_xlabels(xlabel)
+    g.set_ylabels(ylabel)
+    plt.title(title)
+    plt.show()
+
