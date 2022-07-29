9dffecfa3f79935c8e1bb23f095d5b2f8d0a415b
Peter Sidajaya <ptrsdjy@gmail.com>
2022-07-29 12:42:00 +0800

    spin measurements over the Bloch sphere

--- runnable_training.py
+++ runnable_training.py
@@ -16,11 +16,10 @@ config.shuffle_epochs = 1
 config.epochs = 10
 
 folder_name = "qutrit\\"
-state = 1/9 * qt.tensor(qt.identity(3), qt.identity(3))
 ket = (qt.tensor(qt.basis(3,0), qt.basis(3,0)) \
-    + qt.tensor(qt.basis(3,1), qt.basis(3,1)) \
+        + qt.tensor(qt.basis(3,1), qt.basis(3,1)) \
         + qt.tensor(qt.basis(3,2), qt.basis(3,2))).unit()
-state = 0.5 * 1/9 * qt.tensor(qt.identity(3), qt.identity(3)) + 0.5 * qt.ket2dm(ket)
+state = 0.35 * 1/9 * qt.tensor(qt.identity(3), qt.identity(3)) + 0.65 * qt.ket2dm(ket)
 model = build_NewModel_NC()
 print("Model finished.")
 
@@ -30,19 +29,19 @@ K.clear_session()
 
 config.epochs = 25
 minima, history = train_generator(model, create_generator(state, dim=3), save=True,
-                        save_name=folder_name + 'qutrit_5_noise.h5', loss=customLoss_multiple, steps=100)
+                        save_name=folder_name + 'qutrit_35_noise.h5', loss=customLoss_multiple, steps=100)
 minimas.append(minima)
 histories.append(history)
 
-model.save_weights(folder_name + 'weights.h5')
+model.save_weights(folder_name + 'weights7.h5')
 symbolic_weights = getattr(model.optimizer, 'weights')
 weight_values = K.batch_get_value(symbolic_weights)
-with open(folder_name + 'optimizer.pkl', 'wb') as f:
+with open(folder_name + 'optimizer7.pkl', 'wb') as f:
     pickle.dump(weight_values, f)
 
-save_name = folder_name + 'loss.csv'
+save_name = folder_name + 'loss7.csv'
 np.savetxt(save_name, minimas)
-save_name = folder_name + 'history.csv'
+save_name = folder_name + 'history7.csv'
 np.savetxt(save_name, histories[0])
 
 print("Training finished")
