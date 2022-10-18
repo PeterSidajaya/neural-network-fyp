05046e0598a5546f4a053892841c978cef9bab1f
Peter Sidajaya <ptrsdjy@gmail.com>
2022-10-18 15:24:26 +0800

    change runnable

--- runnable_training.py
+++ runnable_training.py
@@ -1,54 +1,37 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-from model_testing import *
-from neural_network_util import build_model_comm, comm_customLoss_multiple, build_NewModel, build_NewModel_Afix
+from neural_network_util import *
 import config
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import qutip as qt
 import pickle
-from distribution_generator_qutrit import *
+from distribution_generator import *
 from training import *
 
-folder_name = "qutrit\\spin measurements - comm\\"
-ket = (qt.tensor(qt.basis(3,0), qt.basis(3,0)) \
-        + qt.tensor(qt.basis(3,1), qt.basis(3,1)) \
-        + qt.tensor(qt.basis(3,2), qt.basis(3,2))).unit()
-state = 0.75 * 1/9 * qt.tensor(qt.identity(3), qt.identity(3)) + 0.25 * qt.ket2dm(ket)
+folder_name = "qutrit\\"
+
 minimas = []
 histories = []
 K.clear_session()
 
-model = build_NewModel()
+model = build_Model_v3()
+keras.utils.plot_model(model, to_file='qutrit_v3.png',show_shapes=True, dpi=300)
 print("Model finished.")
 
-# # LOAD OLD MODEL
-# model = keras.models.load_model(folder_name + '100 runs\\qutrit_no_noise.h5', compile=False)
+ket = (qt.tensor(qt.basis(3,0), qt.basis(3,0)) + qt.tensor(qt.basis(3,1), qt.basis(3,1)) + qt.tensor(qt.basis(3,2), qt.basis(3,2))).unit()
+state = qt.ket2dm(ket)
 
-# config.epochs = 1
-# config.LHV_size = 1
-# alice_measurements = [[1,0,0]]
-# bob_measurements = [[1,0,0]]
-# minima, history = train_generator(model,
-#                                     create_generator_limited(state, alice_measurements, bob_measurements, dim=3),
-#                                     loss=comm_customLoss_multiple, steps=1)
+minimas = []
+histories = []
+K.clear_session()
 
-# # LOAD OLD WEIGHTS
-# with open(folder_name + '100 runs\\optimizer.pkl', 'rb') as f:
-#     weight_values = pickle.load(f)
-# model.optimizer.set_weights(weight_values)
-
-# print("Model finished.")
-
-config.epochs = 100
-config.LHV_size = 1000
-alice_measurements = [[1,0,0], [0,0,1], [1/np.sqrt(2),0,1/np.sqrt(2)], [1/np.sqrt(2),0,-1/np.sqrt(2)],[0,1,0], [0,0,1], [0,1/np.sqrt(2),1/np.sqrt(2)], [0,1/np.sqrt(2),-1/np.sqrt(2)]]
-bob_measurements = [[1,0,0], [0,0,1], [1/np.sqrt(2),0,1/np.sqrt(2)], [1/np.sqrt(2),0,-1/np.sqrt(2)],[0,1,0], [0,0,1], [0,1/np.sqrt(2),1/np.sqrt(2)], [0,1/np.sqrt(2),-1/np.sqrt(2)]]
-minima, history = train_generator(model,
-                                    create_generator_limited(state, alice_measurements, bob_measurements, dim=3), save=True,
-                                    save_name=folder_name + 'qutrit_75_noise.h5', loss=comm_customLoss_multiple, steps=100)
+alice_set = [[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[1,1,1]]
+bob_set = [[1,0,0],[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[1,1,1]]
+minima, history = train_generator(model, create_generator_limited(state, alice_set, bob_set, dim=3), save=True,
+                        save_name=folder_name + 'spin_model.h5', loss=comm_customLoss_multiple, steps=50)
 minimas.append(minima)
 histories.append(history)
 
